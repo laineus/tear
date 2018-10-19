6d22a592f41ab9486eefa1aba23ff52062f1d3b9
laineus <lainen000@gmail.com>
2018-10-19 20:41:43 +0900

    fill arc

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import clm from 'clmtrackr'
 
+const POINTS = {
+  LEFT: {
+    EYE: 28,
+    CORNER: 13
+  },
+  RIGHT: {
+    EYE: 23,
+    CORNER: 1
+  }
+}
+
 const tracker = new clm.tracker()
 tracker.init()
 
@@ -26,8 +37,15 @@ img.onload = () => {
     }
     count ? requestAnimationFrame(() => drawLoop(count - 1, callback)) : callback(pos)
   }
-  drawLoop(100, ret => {
-    console.log(ret)
+  drawLoop(100, pos => {
+    if (!pos) return
+    const key = 'RIGHT'
+    const x = (pos[POINTS[key].EYE][0] + pos[POINTS[key].CORNER][0]) / 2
+    const y = (pos[POINTS[key].EYE][1] + pos[POINTS[key].CORNER][1]) / 2
+    imageCtx.arc(x, y, 2, (0 * Math.PI / 180), (360 * Math.PI / 180), false)
+    imageCtx.fillStyle = 'rgba(15, 10, 0, 0.8)';
+    imageCtx.fill()
+    overlayCtx.clearRect(0, 0, imageCanvas.width, imageCanvas.height)
   })
 }
 img.src = 'sample.jpg'
